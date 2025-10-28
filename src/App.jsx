503d6c9e21d24cd53bbbd59fdaa713ed0280c1d5
Farlina Barik Easha <farlinabarikeasha@Farlinas-MacBook-Pro.local>
2025-10-28 01:02:43 -0700

    my favourite restaurant

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          {'Sushi Go'}<span role="img" aria-label="thumbs up">👍</span>{count}
           count is {count}
         </button>
         <p>
